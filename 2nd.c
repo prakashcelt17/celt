c4195f323c7d0d39b619ae7aec67c14e30174e8b
prakashcelt17 <61412867+prakashcelt17@users.noreply.github.com>
2020-02-24 16:21:45 +0530

    Add files via upload

---
+++ 2nd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("enter the value of a & b");
+scanf("%d%d",&a,&b);
+c=a+b;
+d=a-b;
+printf("sum of 2 no is %d",c);
+printf("diff of 2 no is %d",c);
+}
---
+++ predec.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a=12;
+printf("the value of a is%d\n",a);
+printf("the value of a is%d\n",a--);
+printf("the value of a is%d\n",a);
+}
